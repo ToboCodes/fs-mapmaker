924ab1f45d13e51c97a248757c27f9be3c4d38ca
ToboCodes <ccontreras.cumin@gmail.com>
2023-03-25 15:54:19 -0300

    Add improved location accuracy, marker removal, and update README.md

--- README.md
+++ README.md
@@ -1,53 +1,35 @@
-# FS-MapMaker
+# FS MapMaker
 
-FS-MapMaker is a simple web application that displays a Leaflet map and shows the user's current location using the device's GPS. It also allows you to draw custom polygons directly on the map and get its coords to put them on your map permanently.
+FS MapMaker is an online custom map creation tool that allows users to draw polygons on a map, place markers, and get the coordinates of those markers. Users can also click on existing polygons to view their vertices and modify them.
 
 ## Features
 
-- Shows a map using Leaflet
-- Displays the user's current location with a marker
-- Centers the map to a predefined area if the user is located outside the map bounds
-- Draw and modify custom polygons on the map and get its vertices locations
-
-## Installation
-
-To run this project locally, follow these steps:
-
-1. Clone the repository:
-
-git clone https://github.com/ToboCodes/fs-mapmaker.git
-
-css
-Copy code
-
-2. Navigate to the project folder:
-
-cd fs-mapmaker
-
-markdown
-Copy code
-
-3. Install the required dependencies:
-
-npm install
-
-markdown
-Copy code
-
-4. Start the development server:
-
-npm run start
-
-markdown
-Copy code
-
-5. Open your browser and visit `http://localhost:1234`.
+- Draw polygons by clicking on the map
+- Drag markers to adjust the shape of the polygons
+- Click on existing polygons to place markers on their vertices
+- Remove markers by clicking on them
+- Automatically centers the map on the user's location upon loading (if location permission is granted)
+- Continuously updates the user's location with a GPS marker
 
 ## Dependencies
 
-- [Leaflet](https://leafletjs.com/)
-- [Parcel](https://parceljs.org/)
+- Leaflet.js
+- geolib
+
+## Getting Started
+
+1. Clone the repository.
+2. Run `npm install` to install the necessary dependencies.
+3. Run `npm start` to start the development server.
+4. Open your browser and navigate to `http://localhost:1234`.
+
+## Usage
+
+1. Click on the map to place markers and automatically create a polygon from the markers.
+2. Drag the markers to adjust the shape of the polygon.
+3. Click on an existing polygon to place markers on its vertices. Drag those markers to update the polygon shape.
+4. Click on a marker to remove it.
 
 ## License
 
-This project is open-source and available under the [MIT License](LICENSE).
+This project is open-source and available under the MIT License.
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as L from 'leaflet';
+import { isPointInPolygon } from 'geolib';
 
 // Set map area and center view
 const coord1 = [-39.603457,-73.038868];
@@ -19,61 +20,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(map);
 
-
-// ChatGPT place markers on click and create polygon script 
-let markersArray = [];
-let polygon = null;
-
-map.on('click', function(e) {
-  let Lat = e.latlng.lat;
-  let Lng = e.latlng.lng;
-  let curLocation = [Lat, Lng];
-  let marker = new L.marker(curLocation, {
-    draggable: 'true'
+// Function to add markers to vertices of the polygon
+function addMarkersToVertices(polygon) {
+  const vertices = polygon.getLatLngs()[0];
+  vertices.forEach(vertex => {
+    L.marker([vertex.lat, vertex.lng]).addTo(map);
   });
-  map.addLayer(marker);
-
-  markersArray.push(marker);
-
-  // Output the current positions of all markers
-  console.clear();
-  markersArray.forEach(function(m) {
-    let pos = m.getLatLng();
-    console.log(pos.lat.toFixed(5) + "," + pos.lng.toFixed(5));
-  });
-
-  // Remove the previous polygon (if any) and create a new one
-  if (polygon !== null) {
-    polygon.remove();
-  }
-  let positionsArray = markersArray.map(function(m) {
-    return m.getLatLng();
-  });
-  polygon = L.polygon(positionsArray, {color: 'red', fillOpacity: 0.7, weight: 2}).addTo(map);
-
-  marker.on('dragend', function(event) {
-    let position = marker.getLatLng();
-    marker.setLatLng(position, {
-      draggable: 'true'
-    }).bindPopup(position).update();
-
-    // Clear the console
-    console.clear();
-
-    // Output the current positions of all markers
-    markersArray.forEach(function(m) {
-      let pos = m.getLatLng();
-      console.log(pos.lat.toFixed(5) + "," + pos.lng.toFixed(5));
-    });
-
-    // Update the polygon with the new positions
-    positionsArray = markersArray.map(function(m) {
-      return m.getLatLng();
-    });
-    polygon.setLatLngs(positionsArray);
-  });
-});
-
+}
 
 // Set custom zones
 const territorios = {
@@ -259,13 +212,107 @@ const colors = {
 17: "#EF00F1", 18: "#FF3E3D"
 }
 
-// Place custom zones
+// Updated createZones function
 function createZones(obj, col) {
   for (let i in obj) {
-      let num = i.match(/\d+/)[0];
-      L.polygon(obj[i], {color: col[num], fillOpacity: 0.7, weight: 2}).addTo(map);
+    let num = i.match(/\d+/)[0];
+    let polygon = L.polygon(obj[i], { color: col[num], fillOpacity: 0.7, weight: 2 }).addTo(map);
   }
 }
+
+let markersArray = [];
+let polygon = null;
+
+function createDraggableMarker(latlng) {
+  let marker = new L.marker(latlng, {
+    draggable: 'true'
+  });
+  map.addLayer(marker);
+
+  marker.on('click', function (event) {
+    map.removeLayer(marker);
+    markersArray = markersArray.filter(m => m !== marker);
+
+    // Update the polygon with the new positions
+    if (polygon !== null) {
+      let positionsArray = markersArray.map(function (m) {
+        return m.getLatLng();
+      });
+      polygon.setLatLngs(positionsArray);
+    }
+  });
+
+  marker.on('dragend', function (event) {
+    let position = marker.getLatLng();
+    marker.setLatLng(position, {
+      draggable: 'true'
+    }).bindPopup(position).update();
+
+    // Clear the console
+    console.clear();
+
+    // Output the current positions of all markers
+    markersArray.forEach(function (m) {
+      let pos = m.getLatLng();
+      console.log(pos.lat.toFixed(5) + "," + pos.lng.toFixed(5));
+    });
+
+    // Update the polygon with the new positions
+    if (polygon !== null) {
+      let positionsArray = markersArray.map(function (m) {
+        return m.getLatLng();
+      });
+      polygon.setLatLngs(positionsArray);
+    }
+  });
+
+  return marker;
+}
+
+
+map.on('click', function (e) {
+  let clickedInsidePolygon = false;
+  let clickedPolygon = null;
+  map.eachLayer(function (layer) {
+    if (layer instanceof L.Polygon && layer !== polygon) {
+      const point = { latitude: e.latlng.lat, longitude: e.latlng.lng };
+      const vertices = layer.getLatLngs()[0].map(vertex => {
+        return { latitude: vertex.lat, longitude: vertex.lng };
+      });
+
+      if (isPointInPolygon(point, vertices)) {
+        clickedInsidePolygon = true;
+        clickedPolygon = layer;
+      }
+    }
+  });
+
+  if (clickedInsidePolygon) {
+    markersArray.forEach(marker => map.removeLayer(marker));
+    markersArray = clickedPolygon.getLatLngs()[0].map(vertex => createDraggableMarker(vertex));
+  } else {
+    let marker = createDraggableMarker(e.latlng);
+    markersArray.push(marker);
+
+    // Output the current positions of all markers
+    console.clear();
+    markersArray.forEach(function (m) {
+      let pos = m.getLatLng();
+      console.log(pos.lat.toFixed(5) + "," + pos.lng.toFixed(5));
+    });
+
+    // Remove the previous polygon (if any) and create a new one
+    if (polygon !== null) {
+      polygon.remove();
+    }
+    let positionsArray = markersArray.map(function (m) {
+      return m.getLatLng();
+    });
+    polygon = L.polygon(positionsArray, { color: 'red', fillOpacity: 0.7, weight: 2 }).addTo(map);
+  }
+});
+
+
 
 createZones(territorios, colors)
 
@@ -324,18 +371,27 @@ const markers = {
   }
 
 // Enable device GPS
-map.locate({watch: true, enableHighAccuracy: true, maximumAge: 2000});
+map.locate({
+  watch: true,
+  enableHighAccuracy: true,
+  maximumAge: 1000, // Reduced maximum age to get more frequent updates
+  timeout: 10000, // Increased timeout to allow for more time to get accurate location
+});
 
 // Add location marker
 let gpsMarker = null;
+let hasCentered = false;
 
 function onLocationFound(e) {
   const latlng = e.latlng;
   const mapBounds = L.latLngBounds(coord1, coord2);
 
   // Update the view based on the user's location or the default view
-  const newView = mapBounds.contains(latlng) ? latlng : view;
-  map.setView(newView);
+  if (!hasCentered) {
+    const newView = mapBounds.contains(latlng) ? latlng : view;
+    map.setView(newView);
+    hasCentered = true; // Set the flag to true once the view has been centered
+  }
 
   // Create or update the GPS marker with the user's latitude and longitude
   if (gpsMarker == null) {
@@ -344,7 +400,6 @@ function onLocationFound(e) {
     gpsMarker.setLatLng(latlng);
   }
 }
-
 
 map.on('locationfound', onLocationFound);
 
